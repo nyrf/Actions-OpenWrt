c0995431d53cd270969d96f1bb3d0ba067e8653d
nyrf <xiaojin0913@163.com>
2024-05-17 05:06:58 +0800

    Update diy-part2.sh

--- OpenWrt/diy-part2.sh
+++ OpenWrt/diy-part2.sh
@@ -16,11 +16,11 @@ device_name='OpenWrt'
 sed -i "s/OpenWrt/$device_name/g" ./package/base-files/files/bin/config_generate
 
 echo "修改wifi名称"
-#wifi_name=openwrt
-#sed -i "s/OpenWrt/$wifi_name/g" ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+wifi_name=ysf
+sed -i "s/OpenWrt/$wifi_name/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 
 echo "wifi默认开启"
-# sed -i '/set wireless.${name}.disabled/d' ./package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/set wireless.${name}.disabled/d' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
 
 # # Disable IPV6 ula prefix
 # echo "关闭IPV6"
